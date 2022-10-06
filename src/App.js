bbc0dd250b2857a483d594ac1e72c09b065ae5b2
mingki831 <85330584+mingki831@users.noreply.github.com>
2022-10-06 12:18:34 +0900

    완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+function Block(props) {
+  return (
+    <div className="squareStyle">
+      {props.todo.content}
+    </div>
+  
+  );
+}
 
 function App() {
+
+  //number 초기값
+  let number = 3;
+
+  //초기상태
+  const initialState = {
+    id: 0,
+    content: "",
+  };
+
+  //디폴트 todos
+  const [todos, setTodos] = useState([
+    {id:1, content:"react를 배워봅시다."},
+  ]);
+
+  const [todo, setTodo] = useState(initialState);
+
+  
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setTodo({ ...todo, [name]: value });
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    setTodos([...todos, { ...todo, id: number }]);
+    setTodo(initialState);
+    number++;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layoutCss">
+      <form onSubmit={onSubmitHandler}>
+        <div>
+          <input
+            type="text"
+            name="content"
+            value={todo.content}
+            onChange={onChangeHandler}
+          />
+          <button>추가하기</button>
+        </div>
+      </form>
+
+      <h1>Todo List</h1>
+
+      <div className="formCss">
+        {todos.map((todo) => {
+          return (
+            <Block todo={todo} key={todo.id} setTodos={setTodos}
+              />
+          );
+        })}
+      </div>    
     </div>
   );
 }
